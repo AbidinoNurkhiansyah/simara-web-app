4aa33632ece4ed7361a9dac5781c189611a370a7
Abidino Nurkhiansyah <abidinonurkhiansyah@gmail.com>
2026-06-29 21:27:24 +0700

    style(beranda): fix redundant vertical spacing and update ProgramKerja margin to use container-custom

--- src/pages/Beranda.tsx
+++ src/pages/Beranda.tsx
@@ -7,7 +7,7 @@ import {
 
 export default function Beranda() {
   return (
-    <div className="space-y-8 md:space-y-12 pb-10 sm:pb-20">
+    <div className="pb-0">
       <HeroSection />
       <LayananSection />
       <AlurPendaftaranSection />
